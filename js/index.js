385f2158f32004fc194ed2b27cfaf815bb34f1eb
Sohag Sagar <sagorsohag29@gmail.com>
2022-02-13 19:55:50 +0600

    first commit

---
+++ js/index.js
@@ -0,0 +1,17 @@
+function yesBtn(){
+    window.location.href=('yes.html');
+}
+function valentineYes(){
+    window.location.href=('getGift.html');
+}
+
+function ProposalYes(){
+    window.location.href=('proposal.html');
+}
+
+function homeBtn(){
+    window.location.href=('index.html');
+}
+function backBtn(){
+    window.location.href=('index.html');
+}
